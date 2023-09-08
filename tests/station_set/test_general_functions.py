71e9573b614f32632359fab2f85c4b450f09049b
Alisson Tenório <alissonal@gmail.com>
2023-09-08 16:10:05 -0300

    more tests

--- tests/station_set/test_general_functions.py
+++ tests/station_set/test_general_functions.py
@@ -16,3 +16,16 @@ class GeneralFunctionsTestCase(TestCase):
             custom_dict_path=f"tests/fixtures/"
         )
 
+    def test_station_name_formatter(self):
+        self.assertEqual(self.etl.station_name_formatter("_auckland aerod&rome/ aws"), 'AUCKLAND_AEROD_ROME_AWS')
+
+    def test_should_combine__dataframe_with_remote_old_dataframe(self):
+        d = {'dt': ["2023-06-25", "2023-07-14"], 'col2': [3, 4]}
+        df = pd.DataFrame(data=d)
+        self.assertTrue(self.etl.should_combine__dataframe_with_remote_old_dataframe(df, kalumburu_metadata))
+
+    def test_should_not_combine__dataframe_with_remote_old_dataframe(self):
+        df = pd.DataFrame(data={'dt': ["2023-08-25", "2023-08-29"], 'col2': [3, 4]})
+        self.assertFalse(self.etl.should_combine__dataframe_with_remote_old_dataframe(df, kalumburu_metadata))
+        df = pd.DataFrame(data={'dt': ["2023-08-26", "2023-08-29"], 'col2': [3, 4]})
+        self.assertFalse(self.etl.should_combine__dataframe_with_remote_old_dataframe(df, kalumburu_metadata))
